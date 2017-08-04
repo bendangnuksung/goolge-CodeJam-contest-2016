885d85d75e85143881ee7c352217d781449f3f30
bendangnuksung <bendangnuksungimsong@gmail.com>
2017-08-04 19:36:40 +0530

    Update README and qualification/CountingSheep.py

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This repository is for the Google CodeJam 2016 solutions
 
 #### Qualification Round
 1. [Counting Sheeps](https://code.google.com/codejam/contest/6254486/dashboard#s=p0) : SOLVED
-2. [Revenge of the Pancakes](https://code.google.com/codejam/contest/6254486/dashboard#s=p1) :!SOLVED 
+2. [Revenge of the Pancakes](https://code.google.com/codejam/contest/6254486/dashboard#s=p1) : SOLVED 
 3. [Coin Jam](https://code.google.com/codejam/contest/6254486/dashboard#s=p2) : !SOLVED
 4. [Fractiles](https://code.google.com/codejam/contest/6254486/dashboard#s=p3) : !SOLVED
 
--- qualification/CountingSheep.py
+++ qualification/CountingSheep.py
@@ -1,15 +1,17 @@
-N = int(input())
-digits = [0,1,2,3,4,5,6,7,8,9]
-lastNumber = 0
-for x in range(1,200):
-	temp = x * N
-	if not digits:
-		print (lastNumber)
-		exit()
-	else:
-		for x in map(int, str(temp)):
-			if x in digits:
-				digits.remove(x)
-	lastNumber=temp
-
-print ("INSOMNIA" if digits else lastNumber)
+times= int(input())
+for i in range(times):
+	N = int(input())
+	digits = [0,1,2,3,4,5,6,7,8,9]
+	lastNumber = 0
+	for x in range(1,200):
+		temp = x * N
+		if not digits:
+			print (lastNumber)
+			break
+		else:
+			for x in map(int, str(temp)):
+				if x in digits:
+					digits.remove(x)
+		lastNumber=temp
+	print("#case ", i+1, " :", end='')
+	print ("INSOMNIA" if digits else lastNumber)
