6722c27b3888d12e2d652445aa402e31d3a28625
bendangnuksung <bendangnuksungimsong@gmail.com>
2017-08-06 16:48:57 +0530

    update codeJam.py

--- qualification/codeJam.py
+++ qualification/codeJam.py
@@ -1,8 +1,8 @@
 import math
 
-def prime(data, used_divisor):
+def prime(data):
 	for i in range (2,data):
-		if (data%i==0) and (i not in used_divisor) :
+		if data%i==0:
 			return 1, i
 	return 0,0
 
@@ -10,6 +10,7 @@ times= int(input())
 while(times):
 	N= int(input())
 	J= int(input())
+	# saving all the combination of the input eg: N=3, saving to data= 101,111
 	data=[]
 	length = N-2
 	size_string = math.pow(2,length)
@@ -18,14 +19,14 @@ while(times):
 		data.append('1' + '0'*(length- len(temp))+ temp + '1')
 	counter=0
 
+	print("#case ", (times+1)-times,":")
+	#iterating each data, checking each data is a prime from base: 2-10
 	for x in data:
 		flag=1
 		divisor=[]
-		used_divisor=[]
 		for y in range(2,11):
 			s=int(x,y)
-			p,q= prime(s,used_divisor)
-			used_divisor.append(q)
+			p,q= prime(s)
 			divisor.append(q)
 			if not p:
 				flag=0
@@ -40,6 +41,9 @@ while(times):
 	if counter<J:
 		print('Could not reach by:', J-counter)
 	times -=1
+
+	##output will be slight different as shown in website, but it fulfills all condition. 
+	##output N=16 and J=50 takes a lot of time
 
 
 
